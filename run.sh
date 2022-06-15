9aaa7fdb577c694a687a1b9eff67b3e12194b0d2
Leandro Heck <leoheck@gmail.com>
2022-06-15 10:15:11 -0300

    Uppdate output

--- run.sh
+++ run.sh
@@ -3,13 +3,15 @@
 repo_name="git-secret-test"
 repo_path="/tmp/${repo_name}"
 
-if [[ ! -d "${repo_path}" ]]; then
-	git clone https://github.com/leoheck/${repo_name}.git "${repo_path}"
-	cd "${repo_path}"
+if [ ! -d "${repo_path}" ]; then
+	echo "Cloning ${repo_path} repo ..."
+	git clone -q https://github.com/leoheck/${repo_name}.git "${repo_path}"
+	cd "${repo_paqth}"
 else
+	echo "Updating existing ${repo_path} repo ..."
 	cd "${repo_path}"
-	git stash
-	git pull
+	git stash -q
+	git pull -q
 fi
 
 # Run the test..
